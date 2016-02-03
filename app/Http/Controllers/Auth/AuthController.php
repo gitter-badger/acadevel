3a3537abab97a86f933d2022639a87c5d4254f1b
Michael Telgmann <mt@shopware.com>
2016-02-03 11:58:16 +0100

    fix logout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest', ['except' => 'logout']);
+        $this->middleware('guest', ['except' => 'getLogout']);
         parent::__construct();
     }
 
