9a30069d283f90c660c826df9310a1e5e6b50229
Michael Telgmann <mt@shopware.com>
2016-02-03 16:29:33 +0100

    add score calculation for exam question

--- app/Models/Exam/Question.php
+++ app/Models/Exam/Question.php
@@ -2,33 +2,83 @@
 
 namespace App\Models\Exam;
 
+use App\Models\Question\Answer as OriginAnswer;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
     protected $table = "exam_attendee_question";
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function exam()
     {
         return $this->belongsTo(Attendee::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function answers()
     {
-        return $this->hasMany(Answer::class);
+        return $this->hasMany(Answer::class, 'exam_attendee_question_id');
     }
 
-    public function attendee_questions() {
-        return $this->belongsToMany(\App\Models\Question\Question::class);
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function origin()
+    {
+        return $this->belongsTo(\App\Models\Question\Question::class, 'question_id');
     }
 
+    /**
+     * @return int|float
+     */
     public function getScore()
     {
-        return 100;
-    }
+        $answersFromExam = $this->answers;
+        $answersFromExamCount = count($answersFromExam);
+        $originalAnswers = $this->origin->answers()->where('isCorrect', '=', true)->get();
+        $correctAnswersCount = count($originalAnswers);
+        $correctAnswersFromExamCount = 0;
+        $score = 1;
 
-    public function getPassed()
-    {
-        return $this->score >= 50;
+        /** @var Answer $answer */
+        foreach ($answersFromExam as $answer) {
+            /** @var OriginAnswer $originalAnswer */
+            foreach ($originalAnswers as $originalAnswer) {
+                if ($answer->answer_id == $originalAnswer->id) {
+                    $correctAnswersFromExamCount++;
+                }
+            }
+        }
+
+        if ($correctAnswersFromExamCount == 0) {
+            $score = 0;
+        } elseif ($correctAnswersFromExamCount < $correctAnswersCount) {
+            $diff = $correctAnswersCount - $correctAnswersFromExamCount;
+            for ($i = $diff; $i > 0, $i--;) {
+                $score = $score - 0.5;
+            }
+
+            if ($score < 0) {
+                $score = 0;
+            }
+        }
+
+        if ($score != 0 && $answersFromExamCount > $correctAnswersCount) {
+            $diff = $answersFromExamCount - $correctAnswersCount;
+            for ($i = $diff; $i > 0, $i--;) {
+                $score = $score - 0.5;
+            }
+
+            if ($score < 0) {
+                $score = 0;
+            }
+        }
+
+        return $score;
     }
 }
--- app/Models/Question/Question.php
+++ app/Models/Question/Question.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\Question;
 
+use App\Models\Exam\Attendee;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
@@ -15,6 +16,6 @@ class Question extends Model
     }
 
     public function attendee_questions() {
-        return $this->belongsToMany();
+        return $this->belongsToMany(Attendee::class);
     }
 }
